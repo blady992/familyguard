eed32e979eadde14f383f5adc26cbe90f2a90b22
mryohan <blady992@gmail.com>
2018-04-29 17:23:36 +0200

    dropped support for Spring Data REST

--- data-storage/src/main/java/pl/aagenda/familyguard/storage/repository/ArtifactRepository.java
+++ data-storage/src/main/java/pl/aagenda/familyguard/storage/repository/ArtifactRepository.java
@@ -1,9 +1,9 @@
 package pl.aagenda.familyguard.storage.repository;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import pl.aagenda.familyguard.storage.domain.node.Artifact;
 
-@RepositoryRestResource
+@Repository
 public interface ArtifactRepository extends Neo4jRepository<Artifact, Long> {
 }
