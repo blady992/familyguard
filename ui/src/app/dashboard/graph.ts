b3a733b6cbd4dd9593421e6832ed40b0c808f874
mryohan <blady992@gmail.com>
2018-05-31 18:07:04 +0200

    added link between spouses on UI

--- ui/src/app/dashboard/graph.ts
+++ ui/src/app/dashboard/graph.ts
@@ -1,76 +1,103 @@
-import { Simulation, forceSimulation, forceManyBody, forceLink, forceCenter, forceY } from 'd3';
-import { Person } from './person';
-import { PersonNode } from './person.node';
-import { PersonLink } from './person.link';
-import * as _ from 'underscore';
+import {forceCenter, forceLink, forceManyBody, forceSimulation, forceY, Simulation} from 'd3';
+import {Person} from './person';
+import {PersonNode} from './person.node';
+import {PersonLink} from './person.link';
 
 export class PeopleGraph {
-    private _simulation: Simulation<any, any>;
-    private _nodes: PersonNode[];
-    private _links: PersonLink[];
+  private readonly _simulation: Simulation<any, any>;
+  private readonly _nodes: PersonNode[];
+  private readonly _childLinks: PersonLink[];
+  private readonly _marriageLinks: PersonLink[];
 
-    get simulation() {
-        return this._simulation;
+  get simulation() {
+    return this._simulation;
+  }
+
+  get nodes() {
+    return this._nodes;
+  }
+
+  get childLinks() {
+    return this._childLinks;
+  }
+
+  get marriageLinks() {
+    return this._marriageLinks;
+  }
+
+  constructor(people: Person[]) {
+    this._nodes = people.map(person => new PersonNode(person));
+    this._childLinks = this.initChildLinks(this._nodes);
+    this._marriageLinks = this.initMarriageLinks(this._nodes);
+
+    const generationMap = this.initGenerationMap(people);
+    this._simulation = this.initSimulation(generationMap);
+  }
+
+  private initSimulation(generationMap) {
+    const simulation = forceSimulation()
+      .force('charge', forceManyBody().strength(-300))
+      .force('child_links', forceLink(this._childLinks).id(d => d['id']).strength(0.2))
+      .force('marriage_links', forceLink(this._marriageLinks).id(d => d['id']).strength(0.2))
+      .force('centers', forceCenter(400, 300))
+      .force('sorts', forceY((d) => generationMap[d['person']['id']] * 100).strength(1))
+    ;
+    simulation.nodes(this._nodes);
+    return simulation;
+  }
+
+  private initChildLinks(nodes: PersonNode[]) {
+    const childLinks = [];
+    nodes.forEach(node =>
+      (node.person.children || []).map(childId =>
+        nodes.find(nodeToFind =>
+          nodeToFind.person.id === childId))
+        .forEach(childNode =>
+          childLinks.push(new PersonLink(node, childNode))));
+    return childLinks;
+  }
+
+  private initMarriageLinks(nodes: PersonNode[]) {
+    const marriageLinks: PersonLink[] = [];
+    nodes.forEach(node =>
+      (node.person.husbands || []).concat(node.person.wives || []).map(spouseId =>
+        nodes.find(nodeToFind =>
+          nodeToFind.person.id === spouseId))
+        .forEach(spouseNode => {
+          if (!marriageLinks.find((val) => val.source === node && val.target === spouseNode) && !marriageLinks.find((val) => val.target === node && val.source === spouseNode)) {
+            marriageLinks.push(new PersonLink(node, spouseNode))
+          }
+        }));
+    return marriageLinks;
+  }
+
+  private initGenerationMap(people: Person[]): Map<number, number> {
+    const generationMap = new Map();
+    const peopleCopy = Object.assign([], people);
+    while (peopleCopy.length > 0) {
+      this.generateMapRecursive(generationMap, peopleCopy[0], peopleCopy, 0);
+    }
+    return generationMap;
+  }
+
+  private generateMapRecursive(generationMap: any, person: Person, people: Person[], level: number) {
+    if (!people.includes(person)) {
+      return;
+    }
+    people.splice(people.indexOf(person), 1);
+    generationMap[person.id] = level;
+
+    if (person.father) {
+      this.generateMapRecursive(generationMap, people.find(p => p.id === person.father), people, level - 1);
+    }
+    if (person.mother) {
+      this.generateMapRecursive(generationMap, people.find(p => p.id === person.mother), people, level - 1);
     }
 
-    get nodes() {
-        return this._nodes;
+    if (person.children) {
+      person.children
+        .map(childId => people.find(p => p.id === childId))
+        .forEach(child => this.generateMapRecursive(generationMap, child, people, level + 1));
     }
-
-    get links() {
-        return this._links;
-    }
-
-    constructor(people: Person[]) {
-        this._nodes = people.map(person => new PersonNode(person));
-        this._links = [];
-        this._nodes.forEach(node =>
-            (node.person.children || []).map(childId =>
-                this._nodes.find(nodeToFind =>
-                    nodeToFind.person.id === childId))
-                .forEach(childNode =>
-                    this._links.push(new PersonLink(node, childNode))));
-        const generationMap = this.initGenerationMap(people);
-        this.initSimulation(this._nodes, this._links, generationMap);
-    }
-
-    private initSimulation(nodes: PersonNode[], links: PersonLink[], generationMap) {
-        this._simulation = forceSimulation()
-            .force('charge', forceManyBody().strength(-300))
-            .force('links', forceLink(links).id(d => d['id']).strength(0.2))
-            .force('centers', forceCenter(400, 300))
-            .force('sorts', forceY((d, i, data) => generationMap[d['person']['id']] * 100).strength(1))
-            ;
-        this._simulation.nodes(nodes);
-    }
-
-    private initGenerationMap(people: Person[]): Map<number, number> {
-        const generationMap = new Map();
-        const peopleCopy = Object.assign([], people);
-        while (peopleCopy.length > 0) {
-            this.generateMapRecursive(generationMap, peopleCopy[0], peopleCopy, 0);
-        }
-        return generationMap;
-    }
-
-    private generateMapRecursive(generationMap: any, person: Person, people: Person[], level: number) {
-        if (!people.includes(person)) {
-            return;
-        }
-        people.splice(people.indexOf(person), 1);
-        generationMap[person.id] = level;
-
-        if (person.father) {
-            this.generateMapRecursive(generationMap, people.find(p => p.id === person.father), people, level - 1);
-        }
-        if (person.mother) {
-            this.generateMapRecursive(generationMap, people.find(p => p.id === person.mother), people, level - 1);
-        }
-
-        if (person.children) {
-            person.children
-                .map(childId => people.find(p => p.id === childId))
-                .forEach(child => this.generateMapRecursive(generationMap, child, people, level + 1));
-        }
-    }
+  }
 }
