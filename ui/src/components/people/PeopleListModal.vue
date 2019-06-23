a55acde5726c441c60e0d021895aaf0bb36e203a
mryohan <blady992@gmail.com>
2019-06-23 22:53:18 +0200

    feat: person select now displays a proper modal

---
+++ ui/src/components/people/PeopleListModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="modal fade"
+       tabindex="-1"
+       role="dialog"
+       aria-labelledby="exampleModalLabel"
+       aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="list-group">
+            <a href="#"
+               class="list-group-item list-group-item-action"
+               data-dismiss="modal"
+               v-for="person in people"
+               v-bind:key="person.id"
+               @click="onPersonSelect(person)">
+              {{person.name}}
+            </a>
+          </div>
+          <pagination
+            v-bind:totalPages="totalPages"
+            v-on:page-switch="fetchPeople"/>
+        </div>
+        <div class="modal-footer">
+          <button type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal">Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as axios from 'axios';
+import Pagination from '@/components/utils/Pagination';
+
+export default {
+  name: 'PeopleListModal',
+  components: { Pagination },
+  data() {
+    return {
+      totalPages: 1,
+      people: [],
+    };
+  },
+  mounted() {
+    this.fetchPeople(1);
+  },
+  methods: {
+    fetchPeople(pageNumber) {
+      axios.get('http://localhost:8081/api/v1/people', {
+        params: {
+          page: pageNumber - 1,
+        },
+      })
+        .then((response) => {
+          this.people = response.data.content;
+          this.totalPages = response.data.totalPages;
+        });
+    },
+    onPersonSelect(person) {
+      this.$emit('person-selected', person);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- ui/src/components/people/PersonSelect.vue
+++ ui/src/components/people/PersonSelect.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="input-group mb-3">
-    <input type="text"
-           class="form-control"
-           readonly
-           v-model="personName">
-    <div class="input-group-append">
-      <button class="btn btn-outline-secondary"
-              type="button"
-              @click="onPersonChange()">Clear</button>
-      <button class="btn btn-outline-secondary"
-              type="button">Switch</button>
+  <div>
+    <div class="input-group mb-3">
+      <input type="text"
+             class="form-control"
+             readonly
+             v-model="personName">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary"
+                type="button"
+                @click="onPersonChange()">Clear</button>
+        <button class="btn btn-outline-secondary"
+                type="button"
+                data-toggle="modal"
+                data-target="#peopleListModal">Switch</button>
+      </div>
     </div>
+    <people-list-modal id="peopleListModal" v-on:person-selected="onPersonChange"/>
   </div>
 </template>
 
 <script>
+import PeopleListModal from '@/components/people/PeopleListModal';
+
 export default {
   name: 'PersonSelect',
+  components: { PeopleListModal },
   props: ['value'],
   data() {
     return {
