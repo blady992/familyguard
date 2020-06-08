8c8e1943332ae8c34ceffdf5e86a0b5bd89a7b00
mryohan <blady992@gmail.com>
2020-06-08 23:37:59 +0200

    test: fixed backend tests

--- data-storage/src/test/java/pl/aagenda/familyguard/datastorage/utils/Neo4jTestUtils.java
+++ data-storage/src/test/java/pl/aagenda/familyguard/datastorage/utils/Neo4jTestUtils.java
@@ -1,12 +1,16 @@
 package pl.aagenda.familyguard.datastorage.utils;
 
-import org.neo4j.harness.junit.Neo4jRule;
+import org.neo4j.configuration.helpers.SocketAddress;
+import org.neo4j.harness.junit.rule.Neo4jRule;
+
+import static org.neo4j.configuration.GraphDatabaseSettings.auth_enabled;
+import static org.neo4j.configuration.connectors.BoltConnector.listen_address;
 
 public final class Neo4jTestUtils {
     public static Neo4jRule prepareNeo4jServer() {
         return new Neo4jRule()
-                .withConfig("dbms.security.auth_enabled", "false")
-                .withConfig("dbms.connector.bolt.address", "127.0.0.1:7000");
+                .withConfig(auth_enabled, false)
+                .withConfig(listen_address, new SocketAddress(7123));
     }
 
     private Neo4jTestUtils() {
--- data-storage/src/test/resources/cypher/events.cyp
+++ data-storage/src/test/resources/cypher/events.cyp
@@ -1,4 +1,4 @@
-MERGE (EventWithLocation:Event {name: 'Event with location', description: 'Description of event with location', locationName: 'FTIMS', locationLatitude: 51.747237, locationLongitude: 19.4534342}),
+CREATE (EventWithLocation:Event {name: 'Event with location', description: 'Description of event with location', locationName: 'FTIMS', locationLatitude: 51.747237, locationLongitude: 19.4534342}),
        (EventWithoutLocation:Event {name: 'Event without location', description: 'Description of event without location'}),
        (FirstParticipant:Person {name: 'First participant', sex: 'MALE'}),
        (SecondParticipant:Person {name: 'Second participant', sex: 'FEMALE'}),
